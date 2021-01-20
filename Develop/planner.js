d2c52d9850a311ba37e7029f9b4279fe1877a293
Your NameAlex <alehr45@gmail.com>
2021-01-20 14:30:13 -0600

    added user input forms for each hour of the work day

---
+++ Develop/planner.js
@@ -0,0 +1,13 @@
+
+// var currentDate = document.getElementbyId("currentDay");
+// currentDate.innerHTML = "10";
+ 
+var today = moment().format("MMM Do YY");
+var now = moment().format("H A");
+console.log(today, now);
+
+
+
+
+
+
