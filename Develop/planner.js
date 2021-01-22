f0f15177aff31badc3f15292c8eb15932b48bfaf
Your NameAlex <alehr45@gmail.com>
2021-01-22 11:25:10 -0600

    added hour variables

--- Develop/planner.js
+++ Develop/planner.js
@@ -1,7 +1,14 @@
 
-// var currentDate = document.getElementbyId("currentDay");
-// currentDate.innerHTML = "10";
- 
+var nineAmElement = document.querySelector("#user-input9");
+var tenAmElement = document.querySelector("#user-input10");
+var elevenAmElement = document.querySelector("#user-input11");
+var twelevePmElement = document.querySelector("#user-input12");
+var onePmElement = document.querySelector("#user-input1");
+var twoPmElement = document.querySelector("#user-input2");
+var threePmElement = document.querySelector("#user-input3");
+var fourPmElement = document.querySelector("#user-input4");
+var fivePmElement = document.querySelector("#user-input5");
+
 var today = moment().format("MMM Do YY");
 var now = moment().format("H A");
 console.log(today, now);
